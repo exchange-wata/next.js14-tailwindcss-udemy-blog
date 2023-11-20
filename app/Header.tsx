8b8ec69202b2fc9e61ff1033f4208a4d0a91f5df
exchange-wata <exchange.wata0907@gmail.com>
2023-11-21 08:04:50 +0900

    header component追加&ページ名の変更

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
 import React from "react";
 
-const Header = () => {
-  return (
-    <header>
-      <div>
-        <h1 className="text-2xl font-extrabold">
-          <Link href="/">Next.js14 Blog</Link>
-        </h1>
-      </div>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="py-5 px-10 border-b flex justify-between items-center">
+    <div>
+      <h1 className="text-2xl font-extrabold">
+        <Link href="/">Next.js14 Blog</Link>
+      </h1>
+    </div>
+    <div>
+      <nav className="text-sm font-mediums">
+        <Link href="/articles/new" className="bg-pink-600 px-3 py-3 rounded-md">
+          記事を書く
+        </Link>
+      </nav>
+    </div>
+  </header>
+);
 
 export default Header;
