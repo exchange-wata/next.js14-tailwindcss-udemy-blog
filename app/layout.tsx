55c04560887475b589921a4ddadc17158860e5fd
exchange-wata <exchange.wata0907@gmail.com>
2023-11-23 20:09:35 +0900

    layoutへ追加&短縮記法に変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export const metadata: Metadata = {
   title: "Next.js14 Blog",
@@ -16,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className="container mx-auto bg-slate-700 text-slate-50">
-        <Header></Header>
+        <Header />
         {children}
-        <footer>Footer</footer>
+        <Footer />
       </body>
     </html>
   );
