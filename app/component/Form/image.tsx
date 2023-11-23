e3ba5976b5954444ff977245878817bec94bcbab
exchange-wata <exchange.wata0907@gmail.com>
2023-11-24 00:06:57 +0900

    表示用image componentの切り出し

---
+++ app/component/Form/image.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// TODO:画像部分の処理
+// TODO: リンク先設定
+// TODO: 画像サイズ問題ないかどうか
+const image = () => {
+  return (
+    <Link href={"#"} className="hover:opacity-75">
+      <Image
+        src={"https://source.unsplash.com/collection/1346951/1000*150?sig=1"}
+        alt={""}
+        width={1280}
+        height={300}
+      />
+    </Link>
+  );
+};
+
+export default image;
