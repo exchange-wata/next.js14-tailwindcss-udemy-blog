310ae60ce3f6a58f880f91f69fb85f67a1b1ebc9
exchange-wata <exchange.wata0907@gmail.com>
2023-11-24 13:23:46 +0900

    link componentの修正

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
 import React from "react";
+import LinkForBlog from "./articles/component/ArticleList/LinkForBlog";
 
 const Header = () => (
   <header className="py-5 px-10 border-b flex justify-between items-center">
     <div>
       <h1 className="text-2xl font-extrabold">
-        <Link content={"Next.js14 Blog"} className={null} link={null} />
+        <LinkForBlog text={"Next.js14 Blog"} clazzName={null} articleId={"#"} />
       </h1>
     </div>
     <div>
       <nav className="text-sm font-mediums">
-        <Link
-          content={"記事を書く"}
-          className={["bg-pink-600", "px-3", "py-3", "rounded-md"]}
-          link={"/articles/new"}
+        <LinkForBlog
+          text={"記事を書く"}
+          clazzName={["bg-pink-600", "px-3", "py-3", "rounded-md"]}
+          articleId={"new"}
         />
       </nav>
     </div>
