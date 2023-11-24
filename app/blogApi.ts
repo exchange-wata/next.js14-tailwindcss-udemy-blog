b8cc744e679f74b4cf4750feb67263047c8269e6
exchange-wata <exchange.wata0907@gmail.com>
2023-11-24 14:18:50 +0900

    todo追加

--- app/blogApi.ts
+++ app/blogApi.ts
@@ -2,6 +2,7 @@ import { Article } from "./types";
 
 // TODO: エラーハンドリング
 // TODO: useなんとかの方？
+// TODO: envにしたい
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" }); // SSR
   return res.json();
