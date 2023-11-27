f0c281d16abf9d1c99fad365c9efc56a9f776a66
exchange-wata <exchange.wata0907@gmail.com>
2023-11-27 15:32:51 +0900

    ssr設定

--- app/blogApi.ts
+++ app/blogApi.ts
@@ -4,6 +4,8 @@ import { Article } from "./types";
 // TODO: useなんとかの方？
 // TODO: envにしたい
 export const getAllArticles = async (): Promise<Article[]> => {
-  const res = await fetch("http://localhost:3001/posts", { cache: "no-store" }); // SSR
+  const res = await fetch("http://localhost:3001/posts", {
+    cache: "no-store", // SSR
+  });
   return res.json();
 };
