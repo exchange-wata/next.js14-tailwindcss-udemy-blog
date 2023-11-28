b4723fb72de1e7e6851427b3eac4dd3ac1cfa38f
exchange-wata <exchange.wata0907@gmail.com>
2023-11-28 20:12:07 +0900

    not foundページを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
       <body className="container mx-auto bg-slate-700 text-slate-50">
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow"></main>
           {children}
+          <main className="flex-grow"></main>
           <Footer />
         </div>
       </body>
