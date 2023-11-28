7a7e68df879cb3917e56d4d13758861c224e1d10
exchange-wata <exchange.wata0907@gmail.com>
2023-11-28 12:15:17 +0900

    個別のデータ取得メソッドを実装

--- app/blogApi.ts
+++ app/blogApi.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 // TODO: エラーハンドリング
@@ -8,10 +9,27 @@ export const getAllArticles = async (): Promise<Article[]> => {
     cache: "no-store", // SSR
   });
 
-  if (!res.ok) throw new Error("データ取得に失敗しました。");
+  if (!res.ok) throw new Error("全てのデータ取得に失敗しました。");
 
   // loadingの挙動確認のための遅延措置
   // await new Promise((resolve) => setTimeout(resolve, 1500))
+
+  return res.json();
+};
+
+// TODO: エラーハンドリング
+// TODO: useなんとかの方？
+// TODO: envにしたい
+export const getDetailArticle = async (uuid: string): Promise<Article> => {
+  // const postId = Number(uuid.replace("-", ""));
+  const res = await fetch(`http://localhost:3001/posts/${uuid}}`, {
+    next: { revalidate: 60 }, // ISR
+  });
+
+  if (res.status === 404) notFound();
+  if (!res.ok) throw new Error(`記事識別値${uuid}の取得に失敗しました。`);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return res.json();
 };
