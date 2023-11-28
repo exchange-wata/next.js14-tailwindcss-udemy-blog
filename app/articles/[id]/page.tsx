d54e3f43acc3192ce0b913e02f0312389295a9c1
exchange-wata <exchange.wata0907@gmail.com>
2023-11-28 11:32:04 +0900

    詳細ページの実装

---
+++ app/articles/[id]/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+  id: string;
+};
+
+const Articles = ({ id }: Props) => {
+  return (
+    <div className="max-w-3xl mx-auto p-5">
+      <Image
+        src={"https://source.unsplash.com/collection/1346951/1000*150?sig=1"}
+        alt={""}
+        width={1280}
+        height={300}
+      />
+      <h1 className="text-4xl text-center mb-10">ここがタイトルです。</h1>
+      <div className="text-lg leading-relaxed text-justify">
+        <p>ここが本文です</p>
+      </div>
+    </div>
+  );
+};
+
+export default Articles;
