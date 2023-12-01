54e50e362e9bd1007968ff5ad9b0db7fd28642b8
exchange-wata <exchange.wata0907@gmail.com>
2023-12-01 13:05:48 +0900

    全件取得の書き方変更

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,14 +1,12 @@
 import { supabase } from "@/database/supabase/createClient";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // TODO: TBL名を管理する
   const { data, error } = await supabase.from("posts").select("*");
   if (error) res.status(500).json({ error });
 
   return res.status(200).json(data);
-}
+};
+
+export default handler;
