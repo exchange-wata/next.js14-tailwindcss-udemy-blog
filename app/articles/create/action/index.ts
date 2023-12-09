3739ecb955d5d46e67d2df4e67e8a3c51757c9ae
Nowe-melancholy <jackpot88230021@gmail.com>
2023-12-09 11:11:43 +0900

    不要なconsoleを削除

--- app/articles/create/action/index.ts
+++ app/articles/create/action/index.ts
@@ -6,8 +6,6 @@ import { redirect } from 'next/navigation';
 import { v4 as uuidv4 } from 'uuid';
 
 export const createArticle = async (formData: FormData) => {
-  console.log('formData', formData);
-  console.log('env', process.env.DB_NAME);
   const { error } = await supabase.from(`${process.env.DB_NAME}`).insert([
     {
       id: uuidv4(),
