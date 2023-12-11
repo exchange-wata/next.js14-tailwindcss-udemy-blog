9e1f947bd33c9fef436d6b5e33758082cfbf9542
exchange-wata <exchange.wata0907@gmail.com>
2023-12-11 10:02:28 +0900

    記事投稿をserver actionに変更

---
+++ app/actions/articles/create.ts
@@ -0,0 +1,22 @@
+'use server';
+
+import { supabase } from '@/database/supabase/createClient';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
+
+export const createArticle = async (formData: FormData) => {
+  const { error } = await supabase.from(`${process.env.DB_NAME}`).insert([
+    {
+      id: uuidv4(),
+      title: formData.get('title'),
+      content: formData.get('content'),
+      createdAt: new Date().toISOString(),
+    },
+  ]);
+
+  if (error) throw error;
+
+  revalidatePath('/');
+  redirect('/');
+};
--- app/articles/create/page.tsx
+++ app/articles/create/page.tsx
@@ -1,45 +1,18 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { createArticle } from '@/app/actions/articles/create';
+import { useFormState } from 'react-dom';
 
 const CreateArticlePage = () => {
-  const router = useRouter();
-
-  const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setLoading(true);
-    await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/article/create`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: uuidv4(),
-        title,
-        content,
-        createdAt: new Date().toISOString(),
-      }),
-    });
-
-    setLoading(false);
-
-    router.push('/');
-    router.refresh();
-  };
+  // FIXME: 謎に警告線でるが挙動に問題はない
+  const { pending } = useFormState();
 
   return (
     <div className='min-h-screen py-8 px-4 md:px-12'>
       <h2 className='text-2xl font-bold mb-4'>ブログ新規作成</h2>
       <form
         className='bg-slate-200 p-6 rounded shadow-lg'
-        onSubmit={handleSubmit}
+        action={createArticle}
       >
         <div className='mb-4'>
           <label className='text-gray-700 text-sm font-bold mb-2'>
@@ -48,24 +21,24 @@ const CreateArticlePage = () => {
           <input
             type='text'
             className='shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:line-none'
-            onChange={(e) => setTitle(e.target.value)}
+            name='title'
           />
         </div>
         <div className='mb-4'>
           <label className='text-gray-700 text-sm font-bold mb-2'>本文</label>
           <textarea
             className='shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:line-none'
-            onChange={(e) => setContent(e.target.value)}
+            name='content'
           />
         </div>
         <button
           type='submit'
           className={`py-2 px-4 border rounded-md ${
-            loading
+            pending
               ? ' bg-pink-400 cursor-not-allowed'
               : ' bg-pink-300 hover:bg-pink-400'
           }`}
-          disabled={loading}
+          disabled={pending}
         >
           投稿
         </button>
